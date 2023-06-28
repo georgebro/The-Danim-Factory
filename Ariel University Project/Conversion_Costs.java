bbd1787cf4c9fd61bcc91259d567c5bc0c231c3c
George Brodsky <georgybrodsky@gmail.com>
2023-06-28 14:43:35 +0300

    Update Conversion_Costs.java
    Fixed code

--- Ariel University Project/Conversion_Costs.java
+++ Ariel University Project/Conversion_Costs.java
@@ -1,12 +1,7 @@
-
 public class Conversion_Costs extends Square {
 		int CONVERSION_COSTS;
-			
-			
 		Conversion_Costs(boolean risk, costs b){
 				super(risk,b);
 				CONVERSION_COSTS=b.CONVERSION_BLOCK;
-			}
-
-
+	}
 }
