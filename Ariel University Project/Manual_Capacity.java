25745d2cf3b6e5fa869bef06e434ef197b06130e
George Brodsky <georgybrodsky@gmail.com>
2023-06-28 14:42:03 +0300

    Update Manual_Capacity.java
    Fixed code

--- Ariel University Project/Manual_Capacity.java
+++ Ariel University Project/Manual_Capacity.java
@@ -4,7 +4,6 @@ public class Manual_Capacity extends Square {
 	int FACTORY_PERIOD_ONE;
 	int FACTORY_PERIOD_TWO;
 	int FACTORY_REVENEUE;
-	
 	Manual_Capacity(boolean risk, costs b){
 		super(risk,b);
 		FACTORY_CAPACITY=b.MANUAL_FACTORY_CAPACITY;
@@ -12,15 +11,9 @@ public class Manual_Capacity extends Square {
 		FACTORY_PERIOD_TWO=b.MANUAL_FACTORY_PERIOD_TWO;
 		FACTORY_REVENEUE=b.MANUAL_FACTORY_REVENUE;
 	}
-	
-	public void play(){
-		
-	}
-	
+	public void play(){}
 	public static void main(String[] args){
 		Manual_Capacity m=new Manual_Capacity(true, new denim_costs());
 		System.out.println(m.board.AUTOMATED_FACTORY_CAPACITY);
 	}
-	
-	
 }
