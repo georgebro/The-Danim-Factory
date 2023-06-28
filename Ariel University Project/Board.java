d4f410faaf59e68e00620c42f802a42dc23fda8c
George Brodsky <georgybrodsky@gmail.com>
2023-06-28 14:43:57 +0300

    Update Board.java
    Fixed code

--- Ariel University Project/Board.java
+++ Ariel University Project/Board.java
@@ -1,13 +1,9 @@
 import java.util.Vector;
-
-
 public class Board {
 	Vector<Square> squares;
-	
 	Board(){
 		squares=new Vector<Square>();
 	}
-	
 	void addSquare(Square s){
 		squares.add(s);
 	}
